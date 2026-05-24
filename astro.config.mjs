d7224c56a4e8e53fe6d23bdcf1bc693bd338bbaa
promptdrake <95160973+promptdrake@users.noreply.github.com>
2026-05-25 00:16:12 +0700

    NEW BARU ASDAPKDOKAPWJKD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://aisbirnusantara.com',
+  adapter: cloudflare({
+    imageService: 'passthrough'
+  }),
+  image: {
+    service: passthroughImageService(),
+    remotePatterns: [{ protocol: 'https' }]
+  }
+});
